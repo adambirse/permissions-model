6169849742a4074460058f9bee92d8aeadff99f7
Adam Birse <adambirse@gmail.com>
2023-10-12 08:31:42 +0100

    initial commit.

--- src/domain/errors/ModelNotFoundError.ts
+++
@@ -1,5 +0,0 @@
-export class ModelNotFound extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
--- src/domain/factory/warehouseFactory.ts
+++
@@ -1,5 +0,0 @@
-import { Warehouse } from '../model/warehouse/warehouse';
-
-export const create = (capacity: number) => {
-  return new Warehouse([], capacity);
-};
