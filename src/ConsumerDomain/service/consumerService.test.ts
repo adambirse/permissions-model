c54a891f7104cf86fa734d956fd7bb217ccd9bc0
Adam Birse <adambirse@gmail.com>
2023-10-13 16:00:28 +0100

    refactor

--- src/ConsumerDomain/service/consumerService.test.ts
+++ src/ConsumerDomain/service/consumerService.test.ts
@@ -1,5 +1,4 @@
 import { Organisation } from '../../permissionDomain/model/organisation';
-import { Permission } from '../../permissionDomain/model/permission';
 import { User } from '../../permissionDomain/model/user';
 import { UserService } from '../../permissionDomain/service/userService';
 import { ConsumerService } from './consumerService';
@@ -8,8 +7,8 @@ describe('Consumer service', () => {
   it('can only perform action if a valid user with permission', () => {
     const userService = new UserService();
     const validUser = new User('Valid', new Organisation('org'));
-    validUser.addPermission([new Permission(requiredPermission)]);
     userService.add(validUser);
+    userService.grantPermission(validUser.name, requiredPermission);
     const service = new ConsumerService(userService);
     expect(service.performRestrictedAction(validUser.name, requiredPermission)).toBeTruthy();
   });
