b50d6087724c4bc8378e9106d578b9d5404d9306
Adam Birse <adambirse@gmail.com>
2023-10-13 08:27:54 +0100

    User service additional tests

--- src/domain/service/userService.test.ts
+++ src/domain/service/userService.test.ts
@@ -4,10 +4,28 @@ import { UserService } from './userService';
 
 describe('User service', () => {
   const userService = new UserService();
-  it('Successfully add a user', () => {
+
+
+  it('Successfully add multiple users', () => {
+    const organisation = new Organisation('asd');
+    const user = new User('me', organisation);
+    const him = new User('him', organisation);
+    const her = new User('her', organisation);
+    const them = new User('them', organisation);
+    userService.add(user);
+    userService.add(him);
+    userService.add(her);
+    userService.add(them);
+    expect(userService.get('me')).toBeDefined();
+    expect(userService.get('him')).toBeDefined();
+    expect(userService.get('her')).toBeDefined();
+    expect(userService.get('them')).toBeDefined();
+  });
+
+  it('succesfully handles not finding a user', () => {
     const organisation = new Organisation('asd');
     const user = new User('me', organisation);
     userService.add(user);
-    expect(userService.get('me')).toBeDefined();
+    expect(userService.get('unknown')).toBeUndefined();
   });
 });
